ef42425e670d7db91eb6d7945358c0f4a9b0fbea
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-05-05 16:06:02 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,59 @@
+let sliderImages = document.querySelectorAll(".slide");
+
+// let sliderImages = [
+//   "images/cameron.jpg",
+//   "images/ducks.jpg",
+//   "images / pool.jpg",
+// ];
+
+// let arrowLeft = document.querySelector("#arrow-left");
+const arrowLeft = document.getElementById("arrow-left");
+const arrowRight = document.getElementById("arrow-right");
+let current = 0;
+
+// Clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+
+// Init slider
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+// Show prev
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+// Show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+console.log(arrowRight);
+// Left arrow click
+arrowLeft.addEventListener("click", () => {
+  console.log("left");
+  if (current == 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", () => {
+  console.log("right");
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
---
+++ gallery.js
@@ -0,0 +1,3 @@
+const imgZoom = document.getElementsByClassName("img-zoom");
+
+console.log(imgZoom);
