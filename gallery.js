3a8a2bd487a4ee2588e1188aeaead2362b14ebf9
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-05-10 01:22:29 +0100

    gallery and carousel changes

--- gallery.js
+++ gallery.js
@@ -1,3 +1,25 @@
-const imgZoom = document.getElementsByClassName("img-zoom");
+const container = document.getElementById("imgs");
 
-console.log(imgZoom);
+const images = [
+  "images/cameron.jpg",
+  "images/ducks.jpg",
+  "images/EOSR0109.jpg",
+  "images/EOSR0126.jpg",
+  "images/EOSR0159.jpg",
+  "images/EOSR0183.jpg",
+  "images/EOSR0219 V2.jpg",
+  "images/EOSR0249.jpg",
+  "images/EOSR0252.jpg",
+  "images/EOSR0295.jpg",
+  "images/EOSR0610 - 3.jpg",
+];
+
+function renderPhotos() {
+  for (let i = 0; i < images.length; i++) {
+    container.innerHTML = `<div class="img-container">
+                <img src="${i}" class="img-zoom">
+            </div>`;
+  }
+}
+
+// renderPhotos();
